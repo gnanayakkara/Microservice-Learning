8589d17c6abf9bac802bf7754eec59dd888beb4a
Gayan Nanayakkara <34508294+gnanayakkara@users.noreply.github.com>
2021-09-10 21:59:23 +0400

    Initial Commit for the project
    Initial start for the project

---
+++ config-local/src/test/java/com/gayan/rentcloud/config/local/ConfigLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gayan.rentcloud.config.local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
